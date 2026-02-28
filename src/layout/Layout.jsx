599ba0db180d98ca5f9e017ae4e993b1dcbe82f8
unknown <TT611@ccs.local>
2026-02-28 18:33:21 +0800

    feat[Jacinth]: Updated the Layout.jsx file

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,17 @@
 import {Outlet} from 'react-router'
+import Navbar from './Root/Navbar'
+import Footer from './Root/Footer'
 export default function Layout () {
 
     return (
-        <div className="layout-container">
+        <div className="min-h-screen flex flex-col">
+            <Navbar/>
 
-            <main className="main-content">
+            <main className="main-content flex-grow">
                 <Outlet/>
             </main>
+
+            <Footer/>
         </div>
     )
 
