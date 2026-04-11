db573c674d8a9d3781002fb42b3f486eb98c51b6
Jacinth <jacinthcedricbarral@gmail.com>
2026-04-11 16:06:38 +0800

    feat[Jacinth]: add token storage and logout to auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,15 +1,24 @@
-import api from './backendConnection';
+import api from "./backendConnection";
 
 const authService = {
-    login: async (payload) => {
-        const response = await api.post('auth/login.php', payload);
-        return response.data;
-    },
+  login: async (payload) => {
+    const response = await api.post("auth/login.php", payload);
+    if (response.data.status === "success") {
+      const { token, user } = response.data.data;
+      sessionStorage.setItem("authToken", token);
+      return user;
+    }
+    return response.data;
+  },
 
-    register: async (payload) => {
-        const response = await api.post('auth/register.php', payload);
-        return response.data;
-    },
+  register: async (payload) => {
+    console.log("payload: ", payload);
+    const response = await api.post("auth/register.php", payload);
+    return response.data;
+  },
+  logout: () => {
+    sessionStorage.removeItem("authToken");
+  },
 };
 
 export default authService;
